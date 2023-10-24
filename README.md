336c29907346d17cbb8e623cefc0c2061ad3bb0a
Nick <d2excalibur20@mail.ru>
2023-10-24 18:23:24 +0300

    some changes

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install
+```
+npm install quadratic_equation_func
+```
+# My first package module
+Welcome!
--- npmwork.js
+++
@@ -1,8 +0,0 @@
-const math = require('./math');
-
-const a = 1;
-const b = -3;
-const c = 2;
-
-const solutions = math.solveQuadraticEquation(a, b, c);
-console.log(solutions);
